6457231155b37ff60667f3d1b818a579366e559b
LucasBouchard1 <lucas-2.bouchard@polymtl.ca>
2021-09-21 17:34:26 -0400

    Finit

--- exercice.py
+++ exercice.py
@@ -2,23 +2,31 @@
 # -*- coding: utf-8 -*-
 
 import math
-
 def get_num_letters(text):
-	return 0
+	return len([lettre for lettre in text if 64<ord(lettre)<91 or 96<ord(lettre)<123])
 
 def get_word_length_histogram(text):
-	return [0]
+	text = text.strip().split(" ")
+	histogram = []
+	for mot in text:
+		lenght = get_num_letters(mot)
+		try:
+			histogram[lenght] += 1
+		except:
+			histogram += [0 for x in range(lenght - len(histogram)+1)]
+			histogram[lenght] += 1
+
+	return histogram
 
 def format_histogram(histogram):
-	ROW_CHAR = "*"
+	return "\n".join([f"{x} {'*'*histogram[x]}" for x in range(len(histogram))])
 
-	return ""
+def format_horizontal_histogram(histogram: list) -> str:
+	histogram.pop(0)
+	lenght_hist, max_histogram = len(histogram), max(histogram)
+	return "\n".join(["".join([chr(124 - 92*(histogram[x]<max_histogram-y)) for x in range(lenght_hist)]) for y in range(max_histogram)])+"\n"+"¯"*lenght_hist
 
-def format_horizontal_histogram(histogram):
-	BLOCK_CHAR = "|"
-	LINE_CHAR = "¯"
 
-	return ""
 
 
 if __name__ == "__main__":
